76812375571328e871a9bbdb29b2e33bacf7cd79
mackvv <mackenzievanvliet@gmail.com>
2022-12-09 09:53:05 -0500

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+//used styling imports
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//used react imports
+import {useState, useEffect} from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue} from "firebase/database";
+//used bootstrap styles
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+//image imports
+import humidImg from './img/humidimg.png';
+import tempImg from './img/tempimg.png';
+import hrImg from './img/hrimg.png';
+
+//firebase setup 
+const firebaseConfig = {
+  apiKey: "AIzaSyCE8U2T95yFQklC0jXmHqsmQsWfZouMGTI",
+  authDomain: "finalproject-36266.firebaseapp.com",
+  databaseURL: "https://finalproject-36266-default-rtdb.firebaseio.com",
+  projectId: "finalproject-36266",
+  storageBucket: "finalproject-36266.appspot.com",
+  messagingSenderId: "939801589556",
+  appId: "1:939801589556:web:3e8ae10d99a548e56a1761"
+};
+
+//connect to firebase realtime database 
+initializeApp(firebaseConfig);
+//reference for the database 
+const db = getDatabase();
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    //get the database section where data is stored from the user (esps) that are uploading data
+    const dbRef = ref(db, 'UsersData/eacN42h5UVVCQamxQoTXUjGV9P53');
+    //hold values for output 
+    const [temp, setTemp] = useState("");
+    const [humid, setHumid] = useState("");
+    const [hr, setHr] = useState("");
+    
+
+    // useEffect will update the webpage on change 
+    useEffect(() => {
+      //onValue will update the outputs when the database changes 
+      onValue(dbRef, (snapshot) => {
+        //data from the database in object form
+        const data = snapshot.val();
+        //set output values 
+        setTemp("Temperature: " + data.DHT.temperature);
+        setHumid("Humidity: " + data.DHT.humidity);
+        setHr("Heart Rate: " + data.HeartRate.heartRate);
+      });
+    });
+  
+    //return webpage with images and outputs
+    return (
+     <Container>
+      <Row>
+        <Col md="auto"><img src ={tempImg} alt="Temperature"/></Col>
+        <Col md={4}><h1> {temp}</h1></Col>
+      </Row>
+      <Row>
+        <Col md="auto"><img src ={humidImg} alt="Humidty"/> </Col>
+        <Col md={4} ><h1>{humid} </h1> </Col>
+      </Row>
+      <Row>
+        <Col md="auto"><img src ={hrImg} alt="Heart Rate"/></Col>
+        <Col md={4}><h1>{hr}</h1></Col>
+      </Row>
+    </Container>
+    );
+  }
 
 export default App;
